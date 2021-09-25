39ffa89fabe26d236653f4a4fe9efbca2b348bfb
kiran <kiran.sonne@mobisoftinfotech.com>
2021-09-25 22:33:48 +0530

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Kiran Sonne on 25/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
